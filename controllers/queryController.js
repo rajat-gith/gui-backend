3c80964ff3982590eae268ac3d7d6d894c94847e
rajat-gith <rajatnayak1582002@gmail.com>
2024-11-27 22:26:37 +0530

    modified response structure

--- controllers/queryController.js
+++ controllers/queryController.js
@@ -14,10 +14,10 @@ const connectDatabase = async (req, res) => {
       password,
       database,
     });
-    return res.json({ success: true, message });
+    return res.json({ success: true, data: message });
   } catch (error) {
     console.error("Database Connection Error:", error.message);
-    return res.status(400).json({ success: false, message: error.message });
+    return res.status(400).json({ success: false, data: error.message });
   }
 };
 
@@ -28,7 +28,7 @@ const runQuery = async (req, res) => {
     if (!query) {
       return res
         .status(400)
-        .json({ success: false, message: "Query is required" });
+        .json({ success: false, data: "Query is required" });
     }
 
     const results = await sqlConnector.query(query);
@@ -37,7 +37,7 @@ const runQuery = async (req, res) => {
     console.error("Query Execution Error:", error.message);
     return res
       .status(500)
-      .json({ success: false, message: "Failed to execute query" });
+      .json({ success: false, data: "Failed to execute query" });
   }
 };
 
--- utils/dbConnectorClass.js
+++ utils/dbConnectorClass.js
@@ -23,7 +23,7 @@ class SQLConnector {
         return new Promise((resolve, reject) => {
           this.dbConnection.connect((err) => {
             if (err) {
-              return reject(new Error("Invalid MySQL credentials"));
+              return reject(new Error("Invalid credentials"));
             }
             resolve("MySQL DB Connected");
           });
