d8e93ee843d36a91f86aac98123a4af05135e229
rajat-gith <rajatnayak1582002@gmail.com>
2024-12-30 11:52:57 +0530

    added sample route

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const queryRoutes = require("./routes/queryRoutes");
 const authRoutes = require("./routes/authRoutes");
+
 mongoose
   .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
@@ -13,9 +14,14 @@ mongoose
   .catch((err) => console.error(err));
 
 const app = express();
+
 app.use(cors());
 app.use(bodyParser.json());
 app.use("/api", queryRoutes);
 app.use("/api/auth", authRoutes);
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
 
 module.exports = app;
