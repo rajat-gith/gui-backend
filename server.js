a3e4b4ae23c11ddec606cffe089e4e5105d6291a
rajat-gith <rajatnayak1582002@gmail.com>
2024-12-30 10:20:49 +0530

    bug fixes

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ const app = require("./app");
 
 const HTTP_PORT = process.env.PORT || 80;
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+app.listen(HTTP_PORT, () => {
+  console.log(`Server running on port ${HTTP_PORT}`);
 });
